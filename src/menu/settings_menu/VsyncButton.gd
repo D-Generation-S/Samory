cac7741ad01b9a1589935b6d225e3fdcc9f674da
Xanatos <10531466+XanatosX@users.noreply.github.com>
2024-10-22 22:31:11 +0200

    Fix button logic

--- src/menu/settings_menu/VsyncButton.gd
+++ src/menu/settings_menu/VsyncButton.gd
@@ -3,13 +3,25 @@ extends OptionButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	connect("item_selected", selection_changed)
 
 func settings_loaded(settings: SettingsResource):
+	var selection = 2
 	match settings.vsync_mode:
 		SettingsResource.VSyncModes.VSYNC_ENABLED:
-			selected = 1
+			selection = 1
 		SettingsResource.VSyncModes.VSYNC_DISABLED:
-			selected = 2
+			selection = 2
 		SettingsResource.VSyncModes.VSYNC_ADAPTIVE:
-			selected = 0
+			selection = 0
+	selection_changed(selection)
+
+func selection_changed(new_selection: int):
+	match new_selection:
+		0:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE)
+		1:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+		2:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	selected = new_selection
