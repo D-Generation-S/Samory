628a153cb698ca49b941a835f6c8c7489b1fc2f9
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-03-06 22:32:17 +0100

    feat: Improve game end screen visuals
    Improve the visuals apperance of the game end screen.
    Add a new resource containing a color only, this does make sure
    that the same highlight color is used for all the highlighted labels.
    Add a new shader to change the color of a sprite, this is required to
    recolor the game icons.
    Add color to player or ai icon if it is the player turn.
    Add a new trophy icon to the game end screen to reward players with.
    
    Fix $135

---
+++ src/menu/ColorReplace.gd
@@ -0,0 +1,28 @@
+extends TextureRect
+
+@export var default_color: ColorResource
+@export var highlight_color: ColorResource
+
+var active: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if material is ShaderMaterial:
+		material = material.duplicate()
+		active = true
+		set_color(default_color.get_color())
+		return
+
+func set_default_color():
+	if !active:
+		return
+	set_color(default_color.get_color())
+
+func set_hightlight_color():
+	if !active:
+		return
+	set_color(highlight_color.get_color())
+
+func set_color(color: Color):
+	var color_vector: Vector3 = Vector3(color.r, color.g, color.b)
+	material.set_shader_parameter("replace_color", color_vector)
