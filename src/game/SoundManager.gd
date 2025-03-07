6bc252d71fc74c3a73b1c2f2342dd1b30788094e
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-03-07 20:16:02 +0100

    feat: Add animation to tutorial text
    Add a animation to the tutorial text, making it getting writting.
    Fix some spelling mistakes
    Fix issue with tutorial not loaded on web or mobile.
    Add possiblity on sound manager to change sound volume db
    if sound is requested.

--- src/game/SoundManager.gd
+++ src/game/SoundManager.gd
@@ -6,9 +6,10 @@ class_name SoundManager
 @export_enum("Master", "sfx", "music") var audio_bus: String = "Master"
 
 var sound_stream_node: Node2D
-
+var players = 0
 
 func _ready():
+	process_mode = PROCESS_MODE_ALWAYS
 	sound_stream_node = Node2D.new()
 	add_child(sound_stream_node)
 	
@@ -21,10 +22,12 @@ func _ready():
 
 func create_new_audio_stream_player() -> AudioStreamPlayer2D:
 	var player = AudioStreamPlayer2D.new()
+	player.name = "player_" + str(players)
 	player.bus = audio_bus
+	players += 1
 	return player
 
-func play_sound_effect(sound: AudioStream):
+func play_sound_effect(sound: AudioStream, volume_db: float = 0):
 	var free_audio_stream = null
 	for child in sound_stream_node.get_children():
 		if child is AudioStreamPlayer2D and !child.playing:
@@ -34,6 +37,7 @@ func play_sound_effect(sound: AudioStream):
 		sound_stream_node.add_child(node)
 		free_audio_stream = node
 
+	free_audio_stream.volume_db = volume_db
 	free_audio_stream.stream = sound
 	free_audio_stream.play()
 
