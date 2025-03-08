ab5a187695b69079de716971dfeda8d7470d61b5
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-03-08 19:10:52 +0100

    feat: add toggle and write sound
    Add a toggle sound for all the checkbox like buttons.
    Add a write sound for all the text input fields.
    
    Fix an issue with the deck viewer.
    
    Fix $14o
    Fix $14q
    Fix $14r

--- src/menu/settings/AutoCompleteRound.gd
+++ src/menu/settings/AutoCompleteRound.gd
@@ -1,5 +1,6 @@
-extends CheckButton
+extends ClickableToggle
 
 func settings_loaded(settings: SettingsResource):
 	button_pressed = settings.auto_close_round
 	toggled.emit(button_pressed)
+	starts_ready = true
